ba9ce0e0045e94e22cf9156ced7ceb4846e934f4
kateblbn <125374342+kateblbn@users.noreply.github.com>
2023-06-06 17:45:33 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,11 @@
-const characters = document.querySelector('.counter__wrapper-characters');
-const show_charac = document.querySelector('.show_charac')
 const area = document.querySelector('.area');
-const words = document.querySelector('.show_word');
-const show_paragraph = document.querySelector('.show_paragraph');
-const show_read = document.querySelector('.show_read');
-const test = document.querySelector('.test');
-const out = document.querySelector('.out');
+const add = document.querySelector('.add');
 
 
 area.addEventListener('input', getCharacters);
 
 function sendTest(wordsLength, p, str, readTime) {
-    test.innerHTML = `
+    add.innerHTML = `
     <p>Characters: ${str}</p>
     <p>Total words: ${wordsLength}</p>
     <p>Paragraphs: ${p}</p>
@@ -24,17 +18,13 @@ function sendTest(wordsLength, p, str, readTime) {
 function getCharacters() {
 
     let str = area.value.split('').filter( c => c != ' ').length;
-    show_charac.innerHTML = str;
-    console.log(str);
 
     let wordsLength = area.value.trim().split(/\s+/).length;
-    words.innerHTML = wordsLength;
 
     let p = area.value.split('\n').length;
-    show_paragraph.innerHTML = p ;
 
     let readTime = (wordsLength * 60 ) / 200;
-    show_read.innerHTML = readTime.toFixed(2);
+
     sendTest(wordsLength, p, str, readTime);
 }
 getCharacters();
